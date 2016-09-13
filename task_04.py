0d36254ac33909752611530a817066553b72823f
Sunil Prajapati <nrk_619@yahoo.com>
2016-09-13 09:54:07 +0000

    Setting of functions with and without parameters

---
+++ task_04.py
@@ -0,0 +1,7 @@
+#!/bin/usr/env python
+# -*- coding: utf-8 -*-
+"""A statement of docstring"""
+
+def too_many_kittens(kittens,litterboxes,catfood=bool):
+     print not (litterboxes >= kittens and catfood)
+    
---
+++ task_05.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A good docstring"""
+def defaults(my_optional=True,my_required=None):
+    print my_optional is my_required
